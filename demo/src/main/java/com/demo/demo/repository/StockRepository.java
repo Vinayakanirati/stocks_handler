3d8238106d90f788a4b642fd737844ea582e6d7d
vinayaka <vinayakanirati@gmail.com>
2026-02-18 14:04:48 +0530

    removed backend

--- demo/src/main/java/com/demo/demo/repository/StockRepository.java
+++
@@ -1,5 +0,0 @@
-package com.demo.demo.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.demo.demo.Entity.Stock;
-public interface StockRepository extends JpaRepository<Stock, Long> {
-}
